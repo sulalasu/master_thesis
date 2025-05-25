2e88d62f2e7304ba3d2050ff24b53c60d9f3958f
sulalasu <schaltjahr@hotmail.com>
2025-05-25 14:26:03 +0200

    add thesis/documentation.md

--- pipeline/01_data_prep/data_preparation.py
+++ pipeline/01_data_prep/data_preparation.py
@@ -3,33 +3,15 @@
 import pandas as pd
 from datetime import datetime
 
+pd.set_option("display.max_columns", None)
+
 df = pd.read_csv("data/01_raw_data/testdaten.tsv", sep="\t")
 
 
+
 print("done reading data")
-print(df.head())
 
-# print(df.head())
-# print(df.info())
-
-
-# print(df.describe())
-# print(df.columns)
-
-# print(f"ype: {type(df.iloc[0, 8])}, {df.iloc[:, 8]}")
-
-# date_cols = df.filter(regex="^T_")
-# print(date_cols)
-# print(date_cols.columns)
-# Convert all dates (strings or date types?) to iso (date only, no time)
-# TODO
-
-# iso_tf = date_cols["T_ISO_T"]
-# us_tf = date_cols["T_US"]
-# us_tft = date_cols["T_US_T"]
-
-# print(iso_tf)
-
+# TODO done: change all date formats to iso without time
 # Mapping of column names with their respective format
 date_format_map = {
     "T_XL" : {"unit" : "D", "origin" : "1899-12-30"},
@@ -44,33 +26,19 @@ date_format_map = {
 
 for col, kwargs in date_format_map.items():
     df[col] = pd.to_datetime(df[col], **kwargs).dt.date
-    print(df[col])
-#string ISO with Time to: ISO without time
-#iso_tf = pd.to_datetime(iso_tf, yearfirst=True)
-#string US without time to: ISO without time
 
-#us_tf = pd.to_datetime(us_tf, format="%m-%d-%y")
-#print("us_tf: \n\n")
-#print(us_tf)
-#print(f"type: {type(us_tf.loc(30))}")
 
-#convert XL (Excel) to ISO:
-#t_xl = pd.to_datetime(df["T_XL"],  unit="D", origin="1899-12-30").dt.date #TODO: get citation/check for correct origin date! got it from stackoverflow 38454403
-#print(t_xl)
-
-# t_xl = t_xl.dt.date
-# print("t_xl without time:\n\n")
-# print(t_xl)
-#string US with time to: ISO without time
-
-#pd.to_datetime()
-# print(f"iso type: {type(iso_tf.iloc[8])}")
-
-# print(f"us type: {type(us_tf.iloc[30])}")
-#def convert_ISOTstr_to_ISOdate(col):
-    # Converts a pandas column/series of a string of ISO DATE + TIME string to a ISO datetime object
-    # pass
-
+# TODO Merge all date columns
+# TODO Rename to "DoT" or "Ttansfusion_date"
 # Merge all date columns
-# TODO
+#print(df["ToD_O"])
 
+# TODO Reformat all Rh-Columns
+# TODO Merge/Split Rh/AB0 Columns
+# Reformat Rh-Columns and split/merge into 2 columns: Rh, AB0
+
+#print(df.head())
+print(df.columns)
+print(df["EC_BG_RH"])
+
+ 
