624e34a6e9a000afdc13bc6c6c2093c9c3267306
sulalasu <schaltjahr@hotmail.com>
2025-07-10 21:12:57 +0200

    add cross_validation (expanding and rolling window) ARIMA test code

--- notebooks/2025-07-05-cross-valid-arima.py
+++ notebooks/2025-07-05-cross-valid-arima.py
@@ -140,20 +140,48 @@ def rolling_window(df, train_len: int, test_len: int):
         yield train_set, test_set
 
 
-def expanding_window(df, split_percent: float, pred_size: int):
-    #TODO: move to top of respective class
+def expanding_window(df, split_percent: float, test_len: int):
+    #TODO: move to top of file of respective class file
     from sklearn.model_selection import TimeSeriesSplit
 
     #create expanding window for cross validation.
-    #pass percentage for split in data (0-1), as well as pred_size, which is the number
+    # pass percentage for split in data (0-1), as well as pred_size, which is the number
     # of rows to look ahead.
 
-    split_idx = get_split_index_by_prct(len(df), split_percent)
+    #index where to split/start the expanding window
+    start_idx = get_split_index_by_prct(len(df), split_percent)
+    end_idx = len(df) - test_len + 1
 
-    
+    res = []
+
+    for split_idx in range(start_idx, end_idx):
+        train_set = df.iloc[:split_idx]
+        test_set = df.iloc[split_idx:split_idx+test_len]
+
+        # print(f"\nsplit index: {split_idx}")
+        # print(f"train set ({len(train_set)}):\n{train_set}")
+        # print(f"\ntest_set ({len(test_set)}):\n{test_set}\n")
+
+        # yield train_set, test_set
+        res.append([train_set, test_set])
+    return res
+
+#%%
+#Test expanding window:
+testing_df = pd.DataFrame({'value': list(range(30))}, index=pd.date_range(start='2023-01-01', periods=30, freq='D'))
+sets = expanding_window(testing_df, 0.4, 2)
+print(sets[0])
+print(sets[0][1])
+
+# x = expanding_window(testing_df, 0.4, 2)
+# for z in x:
+#     print(z[0], "\n", z[1])
+#     print("\n\n")
+
 
 
 #%%
+# Test rolling window
 testing_df = pd.DataFrame({'value': list(range(10))}, index=pd.date_range(start='2023-01-01', periods=10, freq='D'))
 x = rolling_window(testing_df, 3, 2)
 for z in x:
@@ -162,9 +190,45 @@ for z in x:
 
 
 #%%
+# ARIMA
+look_ahead = 4
+train_test_sets = expanding_window(df.data['count'], 0.8, test_len=look_ahead)
 
-df.print_head()
 
-print(df.data)
+#Get date range for total prediction interval, create empty df with datetime index
+# print(train_test_sets[0][1].head(1).index)
+# print(train_test_sets[0][1].index.min())
+res_date_range = pd.date_range(start=train_test_sets[0][1].index.min(), end=train_test_sets[-1][1].index.max())
+column_names = [str(num) for num in range(1, look_ahead+1)]
+res_days_ahead_df = pd.DataFrame(index=res_date_range, columns=column_names)
+
+for i, sets in enumerate(train_test_sets):
+    #train_test_sets consists of list of list with [train_set, test_set] == sets
+    model = ARIMA(sets[0], order=(2,1,1))
+    model_fit = model.fit()
+    model_fc = model_fit.forecast(steps=look_ahead)
+
+    for i, _ in enumerate(model_fc):
+        col = str(i+1)
+        row = model_fc.index[i]
+        val = model_fc.iloc[i]
+
+        res_days_ahead_df.at[row, col] = val
+
+
+#%%
+print(len(df.data))
+#%%
+fig, ax = plt.subplots(figsize=(14,8), dpi=300)
+ax = plt.plot(res_days_ahead_df)
+ax = plt.plot(df.data['count'])
+
+ 
+#%%
+print(model_fc)
+print()
+print(model_fc.iloc[0])
+print(model_fc.index[0])
 
 # %%
+def run_ARIMA(df, split_prct: float=0.8, )
